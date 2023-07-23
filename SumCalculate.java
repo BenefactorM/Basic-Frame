60ce9cd849593d5ff479547a80944c7d3e704969
BenefactorM <140325003+BenefactorM@users.noreply.github.com>
2023-07-23 15:31:58 +0200

    Add files via upload

---
+++ SumCalculate.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tut.ac.za.ui;
+
+
+import static java.awt.Color.BLUE;
+import javax.swing.JFrame;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+/**
+ *
+ * @author DELL
+ */
+public class SumCalculate extends JFrame {
+     
+    
+    public void create()
+    {
+   
+        
+        setTitle("BENEFACTOR");
+        setSize(400,400);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(BLUE);
+         setVisible(true);
+    }
+    
+}
